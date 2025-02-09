e425cb2ade032c64dd0cc5d433a7171984b0eeec
Oskar Sadkowski <mrproskar@gmail.com>
2025-02-09 23:45:24 +0100

    tags and lists flags adding logic implemented, basic css, deleting added task

--- frontend/js/to-do.js
+++ frontend/js/to-do.js
@@ -2,6 +2,8 @@ const inputTask = document.querySelector(".input-task");
 const inputDescription = document.querySelector(".input-descr");
 const listContainer = document.getElementById("list-container");
 const tagsContainer = document.querySelector(".row-input-elements");
+const tagList = document.querySelector('.tag-list');
+
 
 const tagOptions = ["Work", "Personal", "Urgent", "Low Priority"];
 const listOptions = ["Shopping", "Chores", "Meetings", "Fitness"];
@@ -20,39 +22,79 @@ inputDescription.addEventListener("input", function () {
     }
 });
 
-function addTask(){
-    if(inputTask.value === ''){
-        alert("you must write somethig");
+function addTask() {
+    if (inputTask.value === '') {
+        alert("You must write something");
     } else {
         let li = document.createElement("li");
-        if (inputDescription.value !== ''){
-            li.innerHTML = `
-                <img class="checkpoint-off-svg" src="/frontend/public/icons/selection-box-off.svg">
-                <img class="checkpoint-on-svg" src="/frontend/public/icons/selection-box-on.svg">
-                <div>
-                    <strong>${inputTask.value}</strong>
-                    <p>${inputDescription.value}</p>
-                </div>
-                <img class="trash-svg" src="/frontend/public/icons/trash.svg">
-                <img class="chevron-right-svg" src="/frontend/public/icons/chevron-right.svg">
-            `;
-        } else {
-            li.innerHTML = `
-                <img class="checkpoint-off-svg" src="/frontend/public/icons/selection-box-off.svg">
-                <img class="checkpoint-on-svg" src="/frontend/public/icons/selection-box-on.svg">
-                ${inputTask.value}
-                <img class="trash-svg" src="/frontend/public/icons/trash.svg">
-                <img class="chevron-right-svg" src="/frontend/public/icons/chevron-right.svg">
-            `;
-        }
-        // li.classList.add("checked");
+
+        // Pobranie wybranych tagów i list (kopiowanie pełnych elementów HTML)
+        let selectedTags = Array.from(document.querySelectorAll(".tag-list .addedTag[style*='background-color: blue']"));
+        let selectedLists = Array.from(document.querySelectorAll(".tag-list .addedTag[style*='background-color: red']"));
+
+        // Tworzenie UL dla tagów i list (jeśli istnieją)
+        let tagsContainer = selectedTags.length > 0 ? document.createElement("ul") : null;
+        let listsContainer = selectedLists.length > 0 ? document.createElement("ul") : null;
+        let tagsDiv = (selectedTags.length > 0 || selectedLists.length > 0) ? document.createElement("div") : null;
+
+        if (tagsContainer) tagsContainer.classList.add("tag-list");
+        if (listsContainer) listsContainer.classList.add("tag-list");
+        if (tagsDiv) tagsDiv.classList.add("tagsDiv")
+
+            if (tagsContainer) {
+                tagsContainer.innerHTML = selectedTags.map(tag => `
+                    <li class="addedTag" style="
+                        background-color: blue;
+                        color: white;
+                        border-radius: 10px;
+                        margin-left: 0px;">
+                        ${tag.textContent.trim()}
+                    </li>
+                `).join('');
+                tagsDiv.appendChild(tagsContainer);
+            }
+            
+            // Dodanie skopiowanych list do nowego UL jako LI
+            if (listsContainer) {
+                listsContainer.innerHTML = selectedLists.map(list => `
+                    <li class="addedTag" style="
+                        background-color: red;
+                        color: white;
+                        border-radius: 2px;
+                        margin-left: 10px;">
+                        ${list.textContent.trim()}
+                    </li>
+                `).join('');
+                tagsDiv.appendChild(listsContainer);
+            }
+
+        // Tworzenie treści zadania
+        let taskContent = `
+            <img class="checkpoint-off-svg" src="/frontend/public/icons/selection-box-off.svg">
+            <img class="checkpoint-on-svg" src="/frontend/public/icons/selection-box-on.svg">
+            <div>
+                <strong class="input-box input-task">${inputTask.value}</strong>
+                ${inputDescription.value ? `<p class="input-box input-descr">${inputDescription.value}</p>` : ''}
+                ${tagsDiv ? tagsDiv.outerHTML : ''}
+            </div>
+            <img class="trash-svg" src="/frontend/public/icons/trash.svg">
+            <img class="chevron-right-svg" src="/frontend/public/icons/chevron-right.svg">
+        `;
+
+        li.innerHTML = taskContent;
+
         listContainer.appendChild(li);
+
+        // Resetowanie inputów
         inputTask.value = "";
         inputDescription.value = "";
-        tagsContainer.style.flexDirection = "row"
-        tagsContainer.style.alignItems = "center"
+
+        // Resetowanie stylów kontenera tagów
+        tagsContainer.style.flexDirection = "row";
+        tagsContainer.style.alignItems = "center";
         tagsContainer.style.gap = "5px";
         inputDescription.style.maxWidth = "14ch";
+        tagList.innerHTML = ''; // Resetowanie listy tagów po dodaniu taska
     }
 }
 
@@ -63,6 +105,7 @@ function toggleInput(type) {
     let dropdown = document.querySelector(type === 'tag' ? '.tag-dropdown' : '.list-dropdown');
     let options = type === 'tag' ? tagOptions : listOptions;
     let markupInput = `<input type="text" class="dynamic-input" name="${type}" placeholder="${type === 'tag' ? 'Enter tag...' : 'Enter list name...'}">`;
+    // let tagList = document.querySelector('.tag-list');
 
     if (!input) {
         container.insertAdjacentHTML("afterbegin", markupInput);
@@ -70,15 +113,39 @@ function toggleInput(type) {
         input.focus();
         tagsContainer.style.flexDirection = 'column';
         tagsContainer.style.alignItems = 'flex-start';
-        dropdown.style.visibility = 'visible';
+        dropdown.style.display = 'flex';
         dropdown.innerHTML = options.map(option => `<button class="dropdown-item" onclick="selectOption('${type}', '${option}')">${option}</button>`).join('');
     } else {
-        dropdown.style.visibility = 'hidden';
-        let markupTag = `<span class="addedTag"> ${type === 'tag' ? `newTag = ${input.value}` : `newList = ${input.value}`} </span>`
+        dropdown.style.display = 'none';
+
+        // Sprawdzenie, czy jest już jakiś element 'list' w ul
+        let firstListItem = tagList.querySelector(".addedTag[style*='background-color: red']");
+
+        // Jeśli dodajemy LIST, sprawdzamy, czy to pierwszy element
+        let isFirstList = firstListItem === null;
+        
+        let markupTag = `<li class="addedTag"
+            style="background-color: ${type === 'tag' ? 'blue' : 'red'};
+            color: white;
+            border-radius: ${type === 'tag' ? '10px' : '2px'};
+            margin-left: ${type === 'list' ? (isFirstList ? '10px' : '0px') : '0px'};">
+            ${input.value}
+        </li>`;
+
         if (input.value === '' && input.name === type) {
             alert(`Write a name of ${type === 'tag' ? 'tag' : 'list'}`);
         } else if (input.value !== '' && input.name === type) {
-            inputDescription.insertAdjacentHTML("afterend", markupTag)
+            if (type === 'list') {
+                // Jeśli dodajemy 'list', dodajemy na koniec
+                tagList.insertAdjacentHTML("beforeend", markupTag);
+            } else {
+                // Jeśli dodajemy 'tag', wstawiamy przed pierwszym 'list' lub na koniec
+                if (firstListItem) {
+                    firstListItem.insertAdjacentHTML("beforebegin", markupTag);
+                } else {
+                    tagList.insertAdjacentHTML("beforeend", markupTag);
+                }
+            }
             input.remove();
         } else if (input.name !== type) {
             input.remove();
@@ -87,18 +154,16 @@ function toggleInput(type) {
     }
 }
 
+
+
 function selectOption(type, value) {
     let input = document.querySelector(type === 'tag' ? '.tag-input-container .dynamic-input' : '.list-input-container .dynamic-input');
-    let description = document.querySelector('.input-descr');
     
     if (input) {
         input.value = value;
     }
-    
-    description.value += description.value ? `, ${value}` : value;
-}
 
-// loadOptions();
+}
 
 document.addEventListener("click", function (event) {
     // Pobranie kontenerów dla tagów i list
@@ -121,21 +186,25 @@ document.addEventListener("click", function (event) {
     // Jeśli kliknięto **poza** tag-inputem, dropdownem i przyciskiem, zamknij je
     if (!clickedTag && tagInput && tagDropdown && !event.target.closest(".dynamic-input, .tag-dropdown, .row-tag-button")) {
         if (tagInput.value.trim() === "") {
-            tagInput.remove(); // Jeśli input jest pusty, usuń go
+            tagInput.remove();
         }
-        tagDropdown.style.visibility = "hidden"; // Ukryj dropdown
+        tagDropdown.style.display = "none";
     }
 
     // Jeśli kliknięto **poza** list-inputem, dropdownem i przyciskiem, zamknij je
     if (!clickedList && listInput && listDropdown && !event.target.closest(".dynamic-input, .list-dropdown, .row-tag-button")) {
         if (listInput.value.trim() === "") {
-            listInput.remove(); // Jeśli input jest pusty, usuń go
+            listInput.remove();
         }
-        listDropdown.style.visibility = "hidden"; // Ukryj dropdown
+        listDropdown.style.display = "hidden";
     }
 });
 
-
+listContainer.addEventListener("click", function (event) {
+    if (event.target.classList.contains("trash-svg")) {
+        event.target.parentElement.remove();
+    }
+});
 
 // 1. po klikniecu na tag button dodac focus na input
 // 2. dodać listę domyślnych wartości po kliknieciu na przycisk
@@ -149,3 +218,6 @@ document.addEventListener("click", function (event) {
 
 //9. Strukturyzacja tasków na katalagoi, tagi i listy oddzilne okno z podzialem i podpisami
 //10. wyszukiawanie po oknie katalgów poszczegolnych tasków i po kliknieciu w rekord zaladowanie odpowiednie go taska w oknie powiekszonym z edycją
+
+
+
